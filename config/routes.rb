1d674c9cff2e94694add6ae13ab93f72f86dd0b5
Brian V. Hughes <brianvh@dartmouth.edu>
2011-07-29 13:24:53 -0400

    Set up known routes, to minimize merge conflicts.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Migrations::Application.routes.draw do
   match '/login' => 'sessions#new', :as => :login
   match '/logout' => 'sessions#destroy', :as => :logout
   match '/auth/:provider/callback' => 'sessions#create'
+
+  root :to => 'users#index'
+
+  resources :groups
+
+  resources :users
+
+  resources :resources
+
+  resources :profiles
+
+  resources :devices
+
+  resources :migrations
+
+  namespace :admin do
+    
+  end
 end
