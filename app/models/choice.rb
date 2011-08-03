acc0f890727b0eecf452d0a8396e00a8cd22bf5c
Alan German <alan.german@dartmouth.edu>
2011-08-03 17:13:28 -0400

    resolve merge conflict

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,8 +1,8 @@
 class Choice < ActiveRecord::Base
   default_scope :order => "sort_order"
-  
+
   def self.to_options_array
-    all.collect { |choice| choice.value }
+    all.collect { |choice| choice.value } << "Other"
   end
-  
+
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,25 @@
 class Device < ActiveRecord::Base
   
   belongs_to :user
+
+  attr_writer :vendor_other
+
+  def vendor_choice # okay
+    return vendor if DeviceVendorChoice.to_options_array.include?(vendor)
+    "Other"
+  end
+    
+  def vendor_choice=(val)
+    if val == "Other"
+      self.vendor = @vendor_other
+    else
+      self.vendor = val
+    end
+  end
+
+  def vendor_other # okay
+    return "" if DeviceVendorChoice.to_options_array.include?(vendor)
+    vendor
+  end
   
 end
