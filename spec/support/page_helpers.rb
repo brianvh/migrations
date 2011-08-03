83d926384ba8e59db1ac91ef4a0b0935e5106e75
Brian V. Hughes <brianvh@dartmouth.edu>
2011-08-03 18:52:54 -0400

    Added several PageHelpers methods for dealing with Group listing view.

--- spec/support/page_helpers.rb
+++ spec/support/page_helpers.rb
@@ -15,4 +15,28 @@ module PageHelpers
     end
   end
 
+  def have_no_group_members
+    PageMatch.match do |m|
+      m.have %(no group members)
+      m.page { within("#group-members") {
+        has_content?('No users assigned to this group.') } }
+    end
+  end
+
+  def have_group_members(num)
+    PageMatch.match do |m|
+      m.have %(#{num} group member#{num == 1 ? '' : 's'})
+      m.page { within("#group-members") {
+        has_selector?(:xpath, './/tr', :count => num+1) } }
+    end
+  end
+
+  def have_group_member(user)
+    PageMatch.match do |m|
+      m.have %(#{user.last_first} as a group member)
+      m.page { within("#group-members") {
+        has_selector?("#user-#{user.id}") } }
+    end
+  end
+
 end
