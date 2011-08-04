e1c4ab98fa9f2e91bd6f3b4c9e70ffcdf4e15bbf
Alan German <alan.german@dartmouth.edu>
2011-08-04 08:21:56 -0400

    fixing "other" choice selections

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -21,6 +21,9 @@ class DevicesController < ApplicationController
   def create
     @user = User.find(session[:user_id])
     @device = Device.new(params[:device])
+    
+    preset_other_fields
+    
     if @device.save
       @user.devices << @device
       flash[:notice] = "Successfully created Device."
@@ -44,12 +47,21 @@ class DevicesController < ApplicationController
     unless @user.is_admin?
       redirect_to user_path @user unless @device.user_id == @user.id
     end
+
+    preset_other_fields
+    
     if @device.update_attributes(params[:device])
       flash[:notice] = "Successfully updated Device."
       redirect_to devices_path
     else
       render :action => 'edit'
     end
+  end
+
+  def preset_other_fields
+    @device.vendor_other = params[:device][:vendor_other]
+    @device.kind_other = params[:device][:kind_other]
+    @device.os_version_other = params[:device][:os_version_other]
   end
 
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -3,10 +3,32 @@ class Device < ActiveRecord::Base
   belongs_to :user
 
   attr_writer :vendor_other
+  attr_writer :kind_other
+  attr_writer :os_version_other
+  
+  def os_version_choice
+    return os_version if OsVersionChoice.to_options_array.include?(os_version)
+    return "Other" unless os_version.blank?
+    nil
+  end
 
-  def vendor_choice # okay
+  def os_version_choice=(val)
+    if val == "Other"
+      self.os_version = @os_version_other
+    else
+      self.vendor = val
+    end
+  end
+
+  def os_version_other
+    return "" if OsVersionChoice.to_options_array.include?(os_version)
+    os_version
+  end
+
+  def vendor_choice
     return vendor if DeviceVendorChoice.to_options_array.include?(vendor)
-    "Other"
+    return "Other" unless vendor.blank? #self.new_record?
+    nil
   end
     
   def vendor_choice=(val)
@@ -17,9 +39,28 @@ class Device < ActiveRecord::Base
     end
   end
 
-  def vendor_other # okay
+  def vendor_other
     return "" if DeviceVendorChoice.to_options_array.include?(vendor)
     vendor
+  end
+  
+  def kind_choice
+    return kind if DeviceKindChoice.to_options_array.include?(kind)
+    return "Other" unless kind.blank?
+    nil
+  end
+    
+  def kind_choice=(val)
+    if val == "Other"
+      self.kind = @kind_other
+    else
+      self.kind = val
+    end
+  end
+
+  def kind_other
+    return "" if DeviceKindChoice.to_options_array.include?(kind)
+    kind
   end
   
 end
