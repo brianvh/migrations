66a7873a6441871cdfed1c6062f45d5e22169f26
Alan German <alan.german@dartmouth.edu>
2011-08-07 22:26:41 -0400

    add profile edit by admin acceptance test; revise profile edit by client test to ensure admin-only content not displayed

--- spec/acceptance/profiles/admin_edit_profile_spec.rb
+++ spec/acceptance/profiles/admin_edit_profile_spec.rb
@@ -1,42 +1,45 @@
 require 'acceptance/acceptance_helper'
 
-feature "Client user editing their Profile" do
+feature "Admin user editing a Client's Profile" do
 
   background do
-    @user = login_as :client
+    @user = login_as :admin
     email_client_choices
   end
 
   subject { page }
   
-  context "GIVEN: I have a Profile in the system" do
-    let(:create_my_profile) { @user.profiles << Factory(:profile) }
+  context "GIVEN: a Profile for a Client exists in the system" do
+    let(:create_client_profile) {
+      @client = Factory(:client)
+      @client.profiles << Factory(:profile)
+      }
     let(:submit) {
-      # check "email_choice_0"
-      # check "email_choice_1"
       select "No", :from => :migrate_oracle_calendar
       click_button "Update Profile"
       }
   
-    before { create_my_profile }  
+    before { create_client_profile }  
     
-    context "WHEN: I visit my Profile page" do
-      before { visit profile_path @user.profiles.first }
+    context "WHEN: I visit the Client's Profile page" do
+      before { visit profile_path @client.profiles.first }
       
-      it { should have_header :profile, "Migration Profile for #{@user.name}" }
-      it { should have_content 'Oracle Calendar to be migrated? Yes'}
-      it { should_not have_content 'Comfort level: 3' }
+      it { should have_header :profile, "Migration Profile for #{@client.name}" }
+      it { should have_content 'Oracle Calendar to be migrated? Yes' }
+      it { should have_content 'Comfort level: 3' }
 
       context "AND: I click the 'Edit Profile' link" do
         before { click_link 'Edit Profile' }
         
         it { should have_select_field :profile, :migrate_oracle_calendar }
+        it { should have_select_field :profile, :comfort_level }
         it { should have_button 'Update Profile' }
         
         context "AND: I change Migrate Oracle Calendar to 'No' and click 'Update Profile'" do
           before { submit }
           
           it { should have_flash_notice "Success" }
+          it { should have_header :profile, "Migration Profile for #{@client.name}" }
           it { should have_content 'Oracle Calendar to be migrated? No'}
         end
       end
