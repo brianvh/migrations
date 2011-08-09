3707fb67cfa12a3e69169712417b04143d98a942
Alan German <alan.german@dartmouth.edu>
2011-08-09 14:20:52 -0400

    resources; require name field; tweak resources#show and #list views

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,4 +2,7 @@ class Resource < ActiveRecord::Base
 	belongs_to :group
 	has_many :ownerships
   has_many :owners, :through => :ownerships, :source => :user
+  
+  validates_presence_of :name, :message => "can't be blank"
+  
 end
