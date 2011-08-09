1171b268f76234ff66eb0690bdcd63b9e51c3853
Alan German <alan.german@dartmouth.edu>
2011-08-09 14:01:49 -0400

    fix authorization block in resources controller methods

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,12 +3,17 @@ class ResourcesController < ApplicationController
     if current_user.is_support?
       @resources = Resource.all
     else
-      @resource = current_user.resources
+      @resources = current_user.resources
     end
   end
 
   def show
-    @resource = Resource.find(params[:id])
+    if current_user.is_support?
+      @resource = Resource.find(params[:id])
+    else
+      @resource = current_user.resources.find(params[:id])
+      redirect_to user_path if @resource.nil?
+    end
   end
 
   def new
@@ -27,11 +32,21 @@ class ResourcesController < ApplicationController
   end
 
   def edit
-    @resource = Resource.find(params[:id])
+    if current_user.is_support?
+      @resource = Resource.find(params[:id])
+    else
+      @resource = current_user.resources.find(params[:id])
+      redirect_to user_path if @resource.nil?
+    end
   end
 
   def update
-    @resource = Resource.find(params[:id])
+    if current_user.is_support?
+      @resource = Resource.find(params[:id])
+    else
+      @resource = current_user.resources.find(params[:id])
+      redirect_to user_path if @resource.nil?
+    end
     if @resource.update_attributes(params[:resource])
       redirect_to @resource, :notice  => "Successfully updated resource."
     else
