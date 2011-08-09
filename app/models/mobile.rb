75f319936dec3d2720a42997143e4cb7b2d8a854
Brian V. Hughes <brianvh@dartmouth.edu>
2011-08-09 16:33:08 -0400

    Cleaned up the devices/index page template.
     * Moved the navigation to the top of the list.
     * Cleaned up HTML in devices/index and devices/_device.
     * Created office_version and current_browser methods on Mobile model, to
       handle the 'N/A' display for devices/_device row partial.

--- app/models/mobile.rb
+++ app/models/mobile.rb
@@ -1,11 +1,19 @@
 class Mobile < Device
-  
+
+  def office_version
+    'N/A'
+  end
+
+  def current_browser
+    'N/A'
+  end
+
   def mobile_vendor_choice
     return vendor if DeviceMobileVendorChoice.to_options_array.include?(vendor)
     return "Other" unless vendor.blank?
     nil
   end
-  
+
   def mobile_vendor_choice=(val)
     if val == "Other"
       self.vendor = @vendor_other
@@ -13,18 +21,18 @@ class Mobile < Device
       self.vendor = val
     end
   end
-  
+
   def mobile_vendor_other
     return "" if DeviceMobileVendorChoice.to_options_array.include?(vendor)
     vendor
   end
-  
+
   def mobile_kind_choice
     return kind if DeviceMobileKindChoice.to_options_array.include?(kind)
     return "Other" unless kind.blank?
     nil
   end
-  
+
   def mobile_kind_choice=(val)
     if val == "Other"
       self.kind = @kind_other
@@ -32,7 +40,7 @@ class Mobile < Device
       self.kind = val
     end
   end
-  
+
   def mobile_kind_other
     return "" if DeviceMobileKindChoice.to_options_array.include?(kind)
     kind
@@ -43,7 +51,7 @@ class Mobile < Device
     return "Other" unless vendor.blank? #self.new_record?
     nil
   end
-    
+
   def vendor_choice=(val)
     if val == "Other"
       self.vendor = @vendor_other
@@ -56,13 +64,13 @@ class Mobile < Device
     return "" if DeviceMobileVendorChoice.to_options_array.include?(vendor)
     vendor
   end
-  
+
   def kind_choice
     return kind if DeviceMobileKindChoice.to_options_array.include?(kind)
     return "Other" unless kind.blank?
     nil
   end
-    
+
   def kind_choice=(val)
     if val == "Other"
       self.kind = @kind_other
@@ -75,5 +83,5 @@ class Mobile < Device
     return "" if DeviceMobileKindChoice.to_options_array.include?(kind)
     kind
   end
-  
+
 end
