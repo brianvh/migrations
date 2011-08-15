be9b03cf7485d263177d2418bc8f4c73f5317383
Brian V. Hughes <brianvh@dartmouth.edu>
2011-08-15 11:57:36 -0400

    Modified groups/show to delegate to contact and members sub-partials.
     * Also modified GroupsController#show to set @members instance variable.
     * Refactored the link_to_function calls to view helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def logout_link
     current_user.nil? ? "" : link_to("Logout #{current_user.name}", logout_path, :id => "logout")
   end
+
+  def show_div_link(label, div)
+    link_to_function label, "$('#{div}').show('slow')"
+  end
+
+  def hide_div_link(label, div)
+    link_to_function label, "$('#{div}').hide('slow')"
+  end
 end
