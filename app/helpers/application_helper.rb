b05cce270b42015abd18c5de494d16e279a971f1
Alan German <alan.german@dartmouth.edu>
2011-08-16 11:45:57 -0400

    user, support and admin-level tabs helpers; remove 'Add Resource' link and resources#new and #create controller actions; set resource master list view to wide layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,29 @@ module ApplicationHelper
   def hide_div_link(label, div)
     link_to_function label, "$('#{div}').hide('slow')"
   end
+  
+  def user_nav_tabs
+    return "" if controller_path == "sessions"
+    content_tag(:li, link_to("Home", root_path), :class => active_tab_class("users"))
+  end
+  
+  def support_nav_tabs
+    return "" if controller_path == "sessions"
+    return "" unless current_user.is_support?
+    content_tag(:li, link_to("Groups", groups_path), :class => active_tab_class("groups", "right")) +
+    content_tag(:li, link_to("Resources", resources_path), :class => active_tab_class("resources", "right"))
+  end
+  
+  def admin_nav_tabs
+    return "" if controller_path == "sessions"
+    return "" unless current_user.is_support?
+    content_tag(:li, link_to("Migrations", '#'), :class => active_tab_class("migrations", "right"))
+  end
+  
+  def active_tab_class(controller_name, classes="")
+    classes = classes.split
+    active = controller.controller_name == controller_name ? "active" : ""
+    (classes << active).join(" ")
+  end
+  
 end
