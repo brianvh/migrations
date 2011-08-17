59adae50ba8334d054c5ee9b1abfe828a90dfe66
Brian V. Hughes <brianvh@dartmouth.edu>
2011-08-17 14:23:22 -0400

    Updated spec/models/user_spec to validate auto-resync with the DND.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,3 +16,27 @@ describe "A new Client User instance" do
     its(:last_first) { should == 'Client' }
   end
 end
+
+describe "An existing User instance, updating from the DND" do
+
+  subject { user }
+
+  let(:last_update) { 1.day.ago }
+  let(:client) { create :client, :uid => 58789, :name => nil }
+  let(:user) { client }
+
+  context "When the user was last updated, yesterday" do
+    before { client.update_attribute :updated_at, last_update }
+
+    its(:needs_resync?) { should be(true) }
+
+    context "and we ask for the DND profile" do
+      let(:client_id) { client.id }
+      let(:user) { User.find client_id }
+
+      before { user.profile }
+
+      its(:needs_resync?) { should be(false) }
+    end
+  end
+end
