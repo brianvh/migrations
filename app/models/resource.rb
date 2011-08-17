71df4f3d8bf9e6ce64d98dbf92921a4069027f5c
Alan German <alan.german@dartmouth.edu>
2011-08-17 10:46:21 -0400

    add/remove secondary owner from resource. Closes #40.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,14 +3,20 @@ class Resource < ActiveRecord::Base
   belongs_to :primary_owner, :class_name => "User", :foreign_key => "primary_owner_id"
   belongs_to :secondary_owner, :class_name => "User", :foreign_key => "secondary_owner_id"
   
-  validates_presence_of :name, :message => "can't be blank"
+  attr_accessor :secondary_owner_name
   
   def primary_owner_name
     primary_owner.nil? ? "" : primary_owner.last_first
   end
   
-  def secondary_owner_name
-    secondary_owner.nil? ? "" : secondary_owner.last_first
+  def secondary_owner_name(last_first=false)
+    return "" unless secondary_owner
+    last_first ? secondary_owner.last_first : secondary_owner.name
+  end
+  
+  def secondary_owner_name=(name)
+    user = User.lookup_by_name(name)
+    self.secondary_owner = User.lookup_by_name(name)
   end
   
 end
