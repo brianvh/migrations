57445e65231d35b34c51dcc9449ae6ba6f66cb33
Alan German <alan.german@dartmouth.edu>
2011-08-20 23:36:57 -0400

    fix resource secondary owner assignment so that error is displayed if attempt to set secondary owner to invalid DND name

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,7 +1,37 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
-describe Resource do
-  it "should be valid" do
-    Resource.new.should be_valid
+describe "Setting secondary owner by name" do
+
+  let(:client) { Client.create(:name => "Alan S. German") }
+  let(:resource) { build(:resource, :secondary_owner => client) }
+  let(:secondary_owner_name) { "alan german" }
+  
+  let(:update) { resource.update_attributes(:secondary_owner_name => secondary_owner_name) }
+  
+  context "with an empty name" do
+
+    subject { resource }
+
+    let(:secondary_owner_name) { '' }
+
+    it { should be_valid }
+    its(:secondary_owner) { should == client }
+    
   end
+    
+  subject { update }
+  
+  context "with a bad dnd name given" do
+    let(:secondary_owner_name) { '20r afhag' }
+    
+    it { should == false }
+
+  end
+  
+  context "with a good dnd name given" do
+    
+    it { should == true }
+    
+  end
+  
 end
