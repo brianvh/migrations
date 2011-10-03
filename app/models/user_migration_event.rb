c8eb0a09dcad128098b56dedc5f983a344ec2a7c
Alan German <alan.german@dartmouth.edu>
2011-10-03 14:42:34 -0400

    back out changes for mailboxtype

--- app/models/user_migration_event.rb
+++ app/models/user_migration_event.rb
@@ -64,8 +64,7 @@ class UserMigrationEvent < MigrationEvent
   end
 
   def move_type
-    # user.mailboxtype == "" ? "imap" : "onprem"
-    user.profile.mailboxtype == "" ? "imap" : "onprem"
+    user.mailboxtype == "" ? "imap" : "onprem"
   end
 
   def include_oc_migration
