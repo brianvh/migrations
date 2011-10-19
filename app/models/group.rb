8373ed9957966391d5c02bdf6213bbc57660c07e
Alan German <alan.german@dartmouth.edu>
2011-10-19 09:25:24 -0400

    fix validations on group create/edit; group.week_of now required since it's used in the invitation email template

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,9 +11,9 @@ class Group < ActiveRecord::Base
   include Groups::Consultants
   include Groups::Calendars
 
-  validates_presence_of :name, :on => :create, :message => "can't be blank"
-  validates_presence_of :week_of, :on => :create, :message => "can't be blank"
-  validates_uniqueness_of :name, :on => :create, :message => "must be unique"
+  validates :name, :presence => { :message => "can't be blank" }
+  validates :week_of, :presence => { :message => "can't be blank" }
+  validates :name, :uniqueness => { :message => "must be unique" }
   validate :valid_deplclasses?, :on => :create
 
   attr_writer :action
