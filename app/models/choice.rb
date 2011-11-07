6fbdf5cfbb346fa245a8c03d5b3bdfdf0cde0738
Alan German <alan.german@dartmouth.edu>
2011-11-07 09:57:32 -0500

    add "Don't know" option to all device-related choice pulldowns; update help text accordingly

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,7 +2,7 @@ class Choice < ActiveRecord::Base
   default_scope :order => "sort_order"
 
   def self.to_options_array
-    all.collect { |choice| choice.value } << "Other"
+    all.collect { |choice| choice.value } << "Other" << "Don't know"
   end
 
 end
--- app/models/mobile_email_client_choice.rb
+++ app/models/mobile_email_client_choice.rb
@@ -1,7 +1,7 @@
 class MobileEmailClientChoice < Choice
   
   def self.to_options_array
-    all.collect { |choice| choice.value }
+    all.collect { |choice| choice.value } << "Don't know"
   end
   
 end
